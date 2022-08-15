a5194cbf9353236e2c09e1c788e3e453db50f0f0
Moshiur R <moshiurraihan95@gmail.com>
2022-08-16 02:43:01 +0600

    work on funtion and object

---
+++ src/js/function.js
@@ -0,0 +1,9 @@
+function simpleFunction(a, b) {
+    return a * b;
+}
+document.write("The Number is " + simpleFunction(15,3) + ".");
+
+function toCelsius(f) {
+    return (5/9) * (f-32);
+}
+document.write(toCelsius(77));
